3c4ee256b8669ac8180f21d8554dc0e2ef59d003
José Miguel <33165751+josemird@users.noreply.github.com>
2025-03-31 09:28:51 +0200

    messages and likes + api routes

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,11 @@
 
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\MessageController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
 //use App\Http\Controllers\AuthController;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
 
 //toDo Crear el controlador y el modelo y ver como implantar el sanctum
 //Route::post('/register', [AuthController::class, 'register']);
