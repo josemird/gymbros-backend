ef68b9cbb92a3318ba0070092215429fdfabd5ab
José Miguel <josemidaw22@gmail.com>
2025-04-02 21:03:38 +0200

    auth add username

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -17,12 +17,15 @@ class AuthController extends Controller
             'name'     => 'required|string|max:255',
             'email'    => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
+            'username' => 'required|string|max:255|unique:users',
         ]);
 
         $user = User::create([
             'name'     => $request->name,
             'email'    => $request->email,
             'password' => Hash::make($request->password),
+            'username' => 'required|string|max:255|unique:users',
+
         ]);
 
         return response()->json([
