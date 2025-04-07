f5f124d356412651b10c011fc519054c2b2759e2
José Miguel <josemidaw22@gmail.com>
2025-04-07 19:10:20 +0200

    sanctum changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,8 +12,15 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'auth' => \App\Http\Middleware\Authenticate::class,
+            'auth:sanctum' => \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        ]);
+
         $middleware->group('api', [
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
